58140607eac14c0d24b17af5232dfd6a6a6ff4fc
kalwasWe3 <60139437+kalwasWe3@users.noreply.github.com>
2021-03-15 21:45:23 +0100

    top margin 2x footer fix v10

--- src/components/global/Footer.js
+++ src/components/global/Footer.js
@@ -9,10 +9,42 @@ import Wrapper from "src/components/global/Wrapper.js";
 import { StaticQuery, graphql } from "gatsby";
 import BodyText from "src/components/typography/BodyText.js";
 
-const StyledWrapper = styled(Wrapper)`
-  margin-top: 0;
+const StyledWrapper = styled.div`
+  width: calc(100vw - ${sizes.bigMargin * 2}rem);
+  margin: 0 auto;
   margin-bottom: 18rem;
-  @media (max-width: ${mediaQuery.laptop}) {
+
+  position: relative;
+  ::after {
+    content: "";
+    position: absolute;
+    top: 0;
+    right: 0;
+    width: 0.1rem;
+    height: 100%;
+    background-image: ${borderStyles.backgroundImageV};
+    background-size: ${borderStyles.backgroundSizeV};
+    z-index: 9;
+  }
+  ::before {
+    content: "";
+    position: absolute;
+    bottom: 0;
+    right: 0;
+    height: 0.1rem;
+    width: 100%;
+    background-image: ${borderStyles.backgroundImageH};
+    background-size: ${borderStyles.backgroundSizeH};
+  }
+  @media (max-width: ${mediaQuery.desktop}) {
+    width: calc(100% - ${sizes.midMargin * 2}rem);
+    margin: 0 ${sizes.midMargin}rem;
+    margin-top: 8rem;
+  }
+  @media (max-width: ${mediaQuery.tablet}) {
+    width: calc(100% - ${sizes.smallMargin * 2}rem);
+    margin: 0 ${sizes.smallMargin}rem;
+    margin-top: 0rem;
     margin-bottom: 5rem;
   }
 `;
